f9c1469f6f993c3eda099eaef7130b9dac1b7ebb
fog6611 <61009173+fog6611@users.noreply.github.com>
2020-12-24 19:40:24 +0300

    1
    1

---
+++ site/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=387shv',
+    'username' => 'nonroot',
+    'password' => '.KVrF5W.',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
